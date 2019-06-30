15b190c45c9e543ce61d0c8a1f31422a437388b6
Sean Hetzel <seanhetzel1@gmail.com>
2019-06-30 22:12:45 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -27,6 +27,8 @@ class String
   end
 
   def count_sentences
-    self.split(". ").count
+    count_sentence = self.split(". ").count + self.split("? ").count + 
+    self.split("! ").count
+    
   end
 end
